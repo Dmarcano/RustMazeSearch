1ad8e7426c7b8c6d7162b24b6f13ad83948410af
Diego <marcanp.diego@gmail.com>
2020-08-27 17:42:06 -0400

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+/**
+ * The job for this program is to read in a .txt file which represents a maze 
+ */
+
+
+
+fn main() {
+    println!("Hello, world!");
+}
---
+++ src/maze_solver/maze_cell.rs
@@ -0,0 +1,5 @@
+struct Position { 
+    x : u64,
+    y: u64, 
+    is_wall : bool 
+}

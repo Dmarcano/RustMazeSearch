0bff5975cf3f761df0fccfca60937745346e9d4a
Diego <marcanp.diego@gmail.com>
2020-08-29 18:16:06 -0400

    Displaying info

---
+++ src/maze_solver/display.rs
@@ -0,0 +1,23 @@
+use super::maze::*;
+
+/**
+ * A struct that carries a reference to a Maze so that current position may be displayed along 
+ * with the maze
+ */
+pub struct MazePositionDisplay<'a> { // we have lifetime of Struct that goes along with the reference
+    maze : &'a Maze,
+    delay : u32
+}
+
+impl<'a> MazePositionDisplay<'a> { 
+    pub fn new(maze :  &'a Maze, delay : u32) -> MazePositionDisplay { 
+        MazePositionDisplay{maze, delay}
+    }
+}
+
+/**
+ * Trait for displaying information based on a given cells
+ */
+pub trait MazeDisplay {
+    fn print_maze_position(cell : &MazePosition);
+}

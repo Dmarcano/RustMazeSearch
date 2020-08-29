e259048bdd4f1a3918f6af6a641d0f7514c885bf
Diego <marcanp.diego@gmail.com>
2020-08-29 14:03:53 -0400

    using traits

--- src/maze_solver/solver.rs
+++ src/maze_solver/solver.rs
@@ -5,26 +5,38 @@ use std::collections::HashSet;
 /**
  * The Exploration trait is responsible for exploring a Maze Interpretation
  */
-pub trait Exploration {
-    fn give(&mut self, val : &MazePosition );
+pub trait Exploration<T> {
+    fn give(&mut self, val : T);
 
-    fn take(&mut self) -> Option<&MazePosition>;
+    fn take(&mut self) -> Option<T>;
+}
+
+impl<T> Exploration<T> for Vec<T> { 
+
+    fn give(&mut self, val : T ){
+        self.push(val);
+    }
+
+    fn take(&mut self) -> Option<T> {
+        self.pop()
+    }
+
 }
 
 
-pub fn search_maze<'a>( mut exploration :  Vec< &'a MazePosition> , maze : &'a Maze ) { 
+pub fn search_maze<'a, T>( mut exploration :  T , maze : &'a Maze ) where T : Exploration<&'a MazePosition> { 
     let mut seen_cells : HashSet<&MazePosition> = HashSet::new();
     let start = maze.get(0, 0);
     seen_cells.insert(start);
-    exploration.push(start);
+    exploration.give(start);
 
-    while let Some(curr_cell) = exploration.pop() { 
+    while let Some(curr_cell) = exploration.take() { 
         println!("{:?}",curr_cell); // perform operation on maze cell
         seen_cells.insert(curr_cell);
         maze.get_neighbors(curr_cell.x, curr_cell.y).iter()
         .filter(|n| !seen_cells.contains(**n) && !n.is_wall)
         .for_each(|neighbor| {
-            exploration.push(neighbor);
+            exploration.give(neighbor);
        });
     }
 }
